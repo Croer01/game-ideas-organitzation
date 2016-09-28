8d60e7ca8225a3fede1c462cefaf5c95fae5d156
Adria Rodriguez <adria_r_p_@hotmail.com>
2016-09-29 00:06:57 +0200

    modify main content and use db to load and save ideas

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,13 +1,30 @@
 /**
  * Created by Adria on 21/08/2016.
  */
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IdeaDatabase} from "../database.service";
 const slash = require('slash');
 
 @Component({
-    moduleId:slash(module.id),
+    moduleId: slash(module.id),
     selector: 'main-content',
     templateUrl: 'main-content.tmpl.html'
 })
-export class MainContentComponent {
+export class MainContentComponent implements OnInit {
+    private newIdea: any;
+    private ideas: Array<any>;
+
+    constructor(private db: IdeaDatabase) {
+    }
+
+    ngOnInit(): void {
+        this.newIdea = {};
+        this.db.loaded.then(() => this.db.findAll().then((ideas)=>this.ideas = ideas));
+    }
+
+    public addIdea() {
+        this.db.insert(this.newIdea).then((idea)=>this.ideas.push(idea));
+        this.newIdea = {};
+    }
+
 }
