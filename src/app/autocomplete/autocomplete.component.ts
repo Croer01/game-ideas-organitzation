a21f1b9c46d4be112fdf2ce04167b169113bb2ad
Adria Rodriguez <adria_r_p_@hotmail.com>
2016-10-02 21:31:05 +0200

    add output autocomplete and fix some bugs related with suggestions

--- src/app/autocomplete/autocomplete.component.ts
+++ src/app/autocomplete/autocomplete.component.ts
@@ -1,8 +1,8 @@
 /**
  * Created by Adria on 21/08/2016.
  */
-import {Component, OnInit, Input} from "@angular/core";
-import {IdeaDatabase} from "../database.service";
+import {Component, OnInit, Input, Output, EventEmitter} from "@angular/core";
+import {IdeaDatabase} from "../persistance/database.service";
 const slash = require('slash');
 
 @Component({
@@ -15,45 +15,48 @@ export class AutoCompleteComponent implements OnInit {
     private newIdea: any;
     private options: Array<any>;
     private isFocus: Boolean;
-    private autocompleteInput: string;
+    private selectedOption: string;
     @Input()
     private showProperty: string;
+    @Output()
+    private onSelected = new EventEmitter(false);
 
     constructor(private db: IdeaDatabase) {
     }
 
     ngOnInit(): void {
         this.newIdea = {};
-        this.db.loaded.then(() => this.db.findAll().then((ideas)=>this.options = ideas));
-        this.closeOptions();
+        this.db.loaded.then(() => this.db.findAll().then((options)=>this.options = options));
+        this.isFocus = false;
     }
 
     public closeOptions(): void {
         this.isFocus = false;
+        this.options = [];
     }
-
 
     public openOptions(): void {
         if (!this.isFocus) {
-            this.db.findAll().then((ideas)=> {
-                this.options = ideas;
-                this.isFocus = true;
-            });
+            this.isFocus = true;
         }
     }
 
     public getOptionContent(option: any): string {
         return this.showProperty ? option[this.showProperty] : option;
-
     }
 
     public selectOption(event: MouseEvent, option: any): void {
         if (event.button === 0) {
-            this.autocompleteInput = this.getOptionContent(option);
+            this.selectedOption = this.getOptionContent(option);
             this.closeOptions();
+            this.onSelected.emit(option);
         }
 
         event.preventDefault();
     }
 
+    public searchOptions(): void {
+        this.db.findByTitle(this.selectedOption).then((options)=>this.options = options);
+        this.openOptions();
+    }
 }
