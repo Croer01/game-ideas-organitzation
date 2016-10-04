34fbe105bccace2f7e1792483ca982a1be7d84e5
Adria Rodriguez <adria_r_p_@hotmail.com>
2016-10-04 21:56:53 +0200

    use Idea object on main content

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,6 +3,7 @@
  */
 import {Component, OnInit} from '@angular/core';
 import {IdeaDatabase} from "../persistance/database.service";
+import {Idea} from "../persistance/Idea";
 const slash = require('slash');
 
 @Component({
@@ -11,26 +12,26 @@ const slash = require('slash');
     templateUrl: 'main-content.tmpl.html'
 })
 export class MainContentComponent implements OnInit {
-    private newIdea: any;
-    private ideas: Array<any>;
+    private newIdea: Idea;
+    private ideas: Array<Idea>;
     private ideaToLink;
 
     constructor(private db: IdeaDatabase) {
     }
 
     public ngOnInit(): void {
-        this.newIdea = {relatedIdeas:[]};
+        this.newIdea = new Idea();
         this.db.loaded.then(() => this.db.findAll().then((ideas)=>this.ideas = ideas));
     }
 
     public addIdea(): void {
         this.db.insert(this.newIdea).then((idea)=>this.ideas.push(idea));
-        this.newIdea = {relatedIdeas:[]};
+        this.newIdea = new Idea();
     }
 
     public linkIdea(): void {
-        if (this.ideaToLink && this.newIdea.relatedIdeas.indexOf(this.ideaToLink) === -1)
-            this.newIdea.relatedIdeas.push(this.ideaToLink._id);
+        if (this.ideaToLink && this.newIdea.canLink(this.ideaToLink))
+            this.newIdea.linkIdea(this.ideaToLink);
     }
 
 }
